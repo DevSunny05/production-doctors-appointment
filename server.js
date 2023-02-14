20d1216579a71c5569e10f3bad6d5a0f9b988b51
sarang <sarang.office01@gmail.com>
2023-02-14 12:22:02 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,53 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const colors=require('colors')
+const morgan=require('morgan')
+const dotenv=require('dotenv')
+const connectDB = require('./config/db')
+const cors=require('cors')
+const path=require('path')
+
+// config
+dotenv.config()
+
+
+
+// mongodb connection
+mongoose.set('strictQuery', true)
+connectDB();
+
+
+// rest obj
+const app=express();
+
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+// routes
+// user routes
+app.use('/api/v1/user',require('./routes/user-routes'))
+
+// addmin routes
+app.use('/api/v1/admin',require('./routes/admin-routes'))
+
+// doctor routes
+app.use('/api/v1/doctor',require('./routes/doctor-routes'))
+
+
+// static
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+//  port
+const port=process.env.PORT || 8080
+
+
+// listen
+app.listen(port,()=>{
+    console.log(`server running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}` .bgCyan.white)
+})
